c0e4ce90c7b594624964e46f5cdc220826129590
Saravanan <saravanan.uiengineer@gmail.com>
2022-12-14 18:43:25 +0530

    nms-app initial start

---
+++ nms-app/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import RegistrationComponent from './containers/registration';
+
+function App() {
+  return (
+    <div>
+      <RegistrationComponent />
+    </div>
+  );
+}
+
+export default App;
